f94182d03bccfbe9eeddadf6b1d72dc4a074d9b0
sb-haz <sb_haz@icloud.com>
2022-06-22 15:08:30 +0100

    Implement Run method

--- src/main/com/sg/classroster/controller/ClassRosterController.java
+++ src/main/com/sg/classroster/controller/ClassRosterController.java
@@ -1,4 +1,46 @@
 package main.com.sg.classroster.controller;
 
+import main.com.sg.classroster.ui.UserIO;
+import main.com.sg.classroster.ui.UserIOConsoleImpl;
+
 public class ClassRosterController {
+
+    private UserIO io = new UserIOConsoleImpl();
+
+    public void run() {
+        boolean keepGoing = true;
+        int menuSelection;
+
+        while (keepGoing) {
+            io.print("Main Menu");
+            io.print("1. List Student IDs");
+            io.print("2. Create new Student");
+            io.print("3. View a Student");
+            io.print("4. Remove a student");
+            io.print("5. Exit");
+
+            menuSelection = io.readInt("Please select from the above choices.", 1, 5);
+            switch (menuSelection) {
+                case 1:
+                    io.print("LIST STUDENTS");
+                    break;
+                case 2:
+                    io.print("CREATE STUDENT");
+                    break;
+                case 3:
+                    io.print("VIEW STUDENT");
+                    break;
+                case 4:
+                    io.print("REMOVE STUDENT");
+                    break;
+                case 5:
+                    keepGoing = false;
+                    break;
+                default:
+                    io.print("UNKNOWN COMMAND");
+            }
+        }
+        io.print("GOOD BYE");
+    }
+
 }
