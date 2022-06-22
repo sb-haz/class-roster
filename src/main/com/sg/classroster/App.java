83f3262a5ed8e57f095cdc59490a94adfdebc5f4
sb-haz <sb_haz@icloud.com>
2022-06-22 15:05:51 +0100

    Create App class

---
+++ src/main/com/sg/classroster/App.java
@@ -0,0 +1,10 @@
+package main.com.sg.classroster;
+
+import main.com.sg.classroster.controller.ClassRosterController;
+
+public class App {
+    public static void main(String[] args) {
+        ClassRosterController controller = new ClassRosterController();
+        controller.run();
+    }
+}
